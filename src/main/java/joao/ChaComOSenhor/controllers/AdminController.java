65005a0dbac951a56fe3dda0702e3d539459170e
João Gabriel Macedo <jgabreumacedo@gmail.com>
2025-06-13 13:38:08 -0300

    feat: temporarily disable ADMIN role authorization in AdminController
    Signed-off-by: João Gabriel Macedo <jgabreumacedo@gmail.com>

--- src/main/java/joao/ChaComOSenhor/controllers/AdminController.java
+++ src/main/java/joao/ChaComOSenhor/controllers/AdminController.java
@@ -15,7 +15,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,7 +25,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/admin")
-@PreAuthorize("hasRole('ADMIN')")
+//@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
     private final DevotionalService devotionalService;
